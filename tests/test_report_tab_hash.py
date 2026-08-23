660b658d68a785e921a4163e370b6e5fe593e076
Mark Watson <56979791+markwatson0126@users.noreply.github.com>
2026-08-23 16:51:51 +0100

    Test validation summary placement in report tab

--- tests/test_report_tab_hash.py
+++ tests/test_report_tab_hash.py
@@ -5,3 +5,13 @@ def test_fault_form_action_preserves_report_tab_fragment():
     template = Path("app/templates/index.html").read_text(encoding="utf-8")
 
     assert 'action="{{ url_for(\'submit_fault\') }}#report-a-fault"' in template
+
+
+def test_validation_summary_is_inside_report_panel():
+    template = Path("app/templates/index.html").read_text(encoding="utf-8")
+
+    report_panel = template.index('id="report-a-fault"')
+    error_summary = template.index('class="govuk-error-summary"')
+    report_heading = template.index('<h2 class="govuk-heading-l">Report a fault</h2>')
+
+    assert report_panel < error_summary < report_heading
